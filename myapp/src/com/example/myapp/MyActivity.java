990da539e4b5f51f0dae07bda15ab21dbacc5d0a
logic-1 <prateek.22gupta@gmail.com>
2014-04-16 02:05:18 +0530

    second commit comment

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -13,7 +13,7 @@ public class MyActivity extends Activity implements View.OnClickListener {
      * Called when the activity is first created.
      */
 
-//    test commit by me
+//    test commit by me--- ghanta me :P hum likhe hai
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
